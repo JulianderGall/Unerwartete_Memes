7f37d5515b653ff0a22507a20a8c0e0f1c47def5
JulianderGall <143714611+JulianderGall@users.noreply.github.com>
2026-03-11 16:29:42 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'develey-meme-v1';
+const CACHE_NAME = 'develey-meme-v2'; // WICHTIG: Version hochzählen!
 
 // Hier listen wir ALLE Dateien auf, die offline verfügbar sein müssen
 const FILES_TO_CACHE = [
@@ -7,9 +7,12 @@ const FILES_TO_CACHE = [
     './style.css',
     './script.js',
     './Develey_Logo_Ecke.png',
-    './Bereit%20für%20das%20%23unerwartete_Störer.png' 
+    './Bereit%20für%20das%20%23unerwartete_Störer.png',
+    './manifest.json',
+    /* NEU: Deine heruntergeladenen Variable-Font-Dateien */
+    './fonts/Montserrat-VariableFont_wght.ttf',
+    './fonts/OpenSans-VariableFont_wdth,wght.ttf'
 ];
-
 // Installation: Dateien herunterladen und in den Cache legen
 self.addEventListener('install', (event) => {
     event.waitUntil(
